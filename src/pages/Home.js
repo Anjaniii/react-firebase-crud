addc45e755aeade2b89c640a70fd4b1b465c8cae
Anjaniii <107039811+Anjaniii@users.noreply.github.com>
2022-06-14 16:05:56 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,30 @@
 import React, { useState, useEffect } from "react";
-import fireDb from 'firebase/compat/app';
+import {db} from './firebase';
 import { Link } from "react-router-dom";
 import "./Home.css";
 import { toast } from "react-toastify";
-
+import {collection, getDocs} from "firebase/firestore";
 
 
 const Home = () => {
-  const [data, setData] = useState({});
-  const [sortedData, setSortedData] = useState([]);
-  const [sort, setSort] = useState(false);
+  const [employees, setEmployees] = useState({});
+  const employeesCollectionRef = collection(db, "employees");
+ 
 
   useEffect(() => {
-    fireDb.child("Employees").on("value", (snapshot) => {
-      if (snapshot.val() !== null) {
-        setData({ ...snapshot.val() });
-      } else {
-        setData({});
-      }
-    });
-
-    return () => {
-      setData({});
-    };
-  }, []);
+    const getEmployees = async () => {
+    const data = await getDocs(employeesCollectionRef);
+    setEmployees(data.docs.map((doc) => ({...doc.data(), id:doc.id})));
+    } 
+  
+    getEmployees();
+  },);
 
   const onDelete = (id) => {
     if (
       window.confirm("Are you sure that you want to delete that Employee ?")
     ) {
-      fireDb.child(`Employees/${id}`).remove((err) => {
+      db.child(`Employees/${id}`).remove((err) => {
         if (err) {
           toast.error(err);
         } else {
@@ -39,71 +34,35 @@ const Home = () => {
     }
   };
 
-  const handleChange = (e) => {
-    setSort(true);
-    fireDb
-      .child("Employees")
-      .orderByChild(`${e.target.value}`)
-      .on("value", (snapshot) => {
-        let sortedData = [];
-        snapshot.forEach((snap) => {
-          sortedData.push(snap.val());
-        });
-        setSortedData(sortedData);
-      });
-  };
-  const handleReset = () => {
-    setSort(false);
-    fireDb.child("Employees").on("value", (snapshot) => {
-      if (snapshot.val() !== null) {
-        setData({ ...snapshot.val() });
-      } else {
-        setData({});
-      }
-    });
-  };
-
-  const filterData = (value) => {
-    fireDb
-      .child("Employees")
-      .orderByChild("status")
-      .equalTo(value)
-      .on("value", (snapshot) => {
-        if (snapshot.val()) {
-          const data = snapshot.val();
-          setData(data);
-        }
-      });
-  };
+  
   return (
     <div style={{ marginTop: "100px" }}>
       <table className="styled-table">
-      <h1> hi</h1>
-
+      
         <thead>
           <tr>
-            <th style={{ textAlign: "center" }}>s.No</th>
+            <th style={{ textAlign: "center" }}>EmpNo</th>
             <th style={{ textAlign: "center" }}>FirstName</th>
             <th style={{ textAlign: "center" }}>LastName</th>
             <th style={{ textAlign: "center" }}>Address</th>
             <th style={{ textAlign: "center" }}>PhoneNo</th>
             <th style={{ textAlign: "center" }}>AadharNo</th>
             <th style={{ textAlign: "center" }}>PanNo</th>
-            {!sort && <th style={{ textAlign: "center" }}>Action</th>}
           </tr>
         </thead>
-        {!sort && (
+        
           <tbody>
-            {Object.keys(data).map((id, index) => {
+            {Object.keys(employees).map((id, index) => {
               return (
                 <tr key={id}>
                   <th scope="row">{index + 1}</th>
-                  <td>{data[id].FirstName}</td>
-                  <td>{data[id].LastName}</td>
-                  <td>{data[id].Address}</td>
-                  <td>{data[id].PhoneNo}</td>
-                  <td>{data[id].AadharNo}</td>
-                  <td>{data[id].PanNo}</td>
+                  <td>{employees[id].EmpNo}</td>
+                  <td>{employees[id].FirstName}</td>
+                  <td>{employees[id].LastName}</td>
+                  <td>{employees[id].Address}</td>
+                  <td>{employees[id].PhoneNo}</td>
+                  <td>{employees[id].AadharNo}</td>
+                  <td>{employees[id].PanNo}</td>
                   <td>
                     <Link to={`/update/${id}`}>
                       <button className="btn btn-edit">Edit</button>
@@ -120,53 +79,13 @@ const Home = () => {
                   </td>
                 </tr>
               );
-            })}
+              })}
           </tbody>
-        )}
-        {sort && (
-          <tbody>
-            {sortedData.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <th scope="row">{index + 1}</th>
-                  <td>{item.FirstName}</td>
-                  <td>{item.LastName}</td>
-                  <td>{item.Address}</td>
-                  <td>{item.PhoneNo}</td>
-                  <td>{item.AadharNo}</td>
-                  <td>{item.PanNo}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        )}
-      </table>
-      <label>Sort By:</label>
-      <select className="dropdown" name="colValue" onChange={handleChange}>
-        <option>Please Select</option>
-        <option value="FirstName">FirstName</option>
-        <option value="LastName">LastName</option>
-        <option value="Address">Address</option>
-        <option value="PhoneNo">PhoneNo</option>
-        <option value="AadharNo">AadharNo</option>
-        <option value="PanNo">PanNo</option>
-      </select>
-      <button className="btn btn-reset" onClick={handleReset}>
-        Reset
-      </button>
-      <br />
-      <label>Status:</label>
-      <button className="btn btn-active" onClick={() => filterData("Active")}>
-        Active
-      </button>
-      <button
-        className="btn btn-inactive"
-        onClick={() => filterData("Inactive")}
-      >
-        Inactive
-      </button>
-    </div>
+          </table>
+          </div>
+        
+        
   );
-};
+}; 
 
 export default Home;
